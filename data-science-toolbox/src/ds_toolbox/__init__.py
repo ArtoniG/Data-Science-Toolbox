a36b905731ccaa5cbbe8e8dd437664598fb880c3
ArtoniG <51254087+ArtoniG@users.noreply.github.com>
2026-09-24 21:31:17 -0500

    Atualizar o __init__.py

--- data-science-toolbox/src/ds_toolbox/__init__.py
+++ data-science-toolbox/src/ds_toolbox/__init__.py
@@ -1,1 +1,7 @@
 __version__ = "0.1.0"
+
+# Expose clean public interface
+from credit_toolbox.metrics.supervised import calculate_ks, calculate_gini
+from credit_toolbox.logging.logger import get_logger
+
+__all__ = ["calculate_ks", "calculate_gini", "get_logger"]
